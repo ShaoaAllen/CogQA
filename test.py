7ca306298bdae53cd7512f90e219733a99105d27
QiTianM425 <417945175@qq.com>
2020-12-24 22:03:54 +0800

    20201224

--- test.py
+++ test.py
@@ -21,9 +21,9 @@ from data import convert_question_to_samples_bundle, homebrew_data_loader
 
 def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient_accumulation_steps, lr1, lr2, alpha):
     '''Train Sys1 and Sys2 models.
-    
-    Train models by task #1(tensors) and task #2(bundle). 
-    
+
+    Train models by task #1(tensors) and task #2(bundle).
+
     Args:
         bundles (list): List of bundles.
         model1 (BertForMultiHopQuestionAnswering): System 1 model.
@@ -32,11 +32,11 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
         model2 (CognitiveGNN): System 2 model.
         batch_size (int): Defaults to 4.
         num_epoch (int): Defaults to 1.
-        gradient_accumulation_steps (int): Defaults to 1. 
+        gradient_accumulation_steps (int): Defaults to 1.
         lr1 (float): Defaults to 1e-4. Learning rate for Sys1.
         lr2 (float): Defaults to 1e-4. Learning rate for Sys2.
         alpha (float): Defaults to 0.2. Balance factor for loss of two systems.
-    
+
     Returns:
         ([type], [type]): Trained models.
     '''
@@ -49,11 +49,11 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
     optimizer_grouped_parameters = [
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-    num_batch, dataloader = homebrew_data_loader(bundles, mode = mode, batch_size=batch_size)
+    ]
+    num_batch, dataloader = homebrew_data_loader(bundles, mode=mode, batch_size=batch_size)
     num_steps = num_batch * num_epoch
     global_step = 0
-    opt1 = BertAdam(optimizer_grouped_parameters, lr = lr1, warmup = 0.1, t_total=num_steps)
+    opt1 = BertAdam(optimizer_grouped_parameters, lr=lr1, warmup=0.1, t_total=num_steps)
     model1.to(device)
     model1.train()
 
@@ -62,15 +62,15 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
         opt2 = Adam(model2.parameters(), lr=lr2)
         model2.to(device)
         model2.train()
-        warmed = False # warmup for jointly training
+        warmed = False  # warmup for jointly training
 
-    for epoch in trange(num_epoch, desc = 'Epoch'):
+    for epoch in trange(num_epoch, desc='Epoch'):
         ans_mean, hop_mean = WindowMean(), WindowMean()
         opt1.zero_grad()
         if mode == 'bundle':
             final_mean = WindowMean()
             opt2.zero_grad()
-        tqdm_obj = tqdm(dataloader, total = num_batch)
+        tqdm_obj = tqdm(dataloader, total=num_batch)
 
         for step, batch in enumerate(tqdm_obj):
             try:
@@ -88,7 +88,7 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
 
                 if (step + 1) % gradient_accumulation_steps == 0:
                     # modify learning rate with special warm up BERT uses. From BERT pytorch examples
-                    lr_this_step = lr1 * warmup_linear(global_step/num_steps, warmup = 0.1)
+                    lr_this_step = lr1 * warmup_linear(global_step / num_steps, warmup=0.1)
                     for param_group in opt1.param_groups:
                         param_group['lr'] = lr_this_step
                     global_step += 1
@@ -99,7 +99,7 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
                         tqdm_obj.set_description('ans_loss: {:.2f}, hop_loss: {:.2f}, final_loss: {:.2f}'.format(
                             ans_mean.update(ans_loss.item()), hop_mean.update(hop_loss.item()), final_mean_loss))
                         # During warming period, model1 is frozen and model2 is trained to normal weights
-                        if final_mean_loss < 0.9 and step > 100: # ugly manual hyperparam
+                        if final_mean_loss < 0.9 and step > 100:  # ugly manual hyperparam
                             warmed = True
                         if warmed:
                             opt1.step()
@@ -111,22 +111,21 @@ def train(bundles, model1, device, mode, model2, batch_size, num_epoch, gradient
                             ans_mean.update(ans_loss.item()), hop_mean.update(hop_loss.item())))
                     if step % 1000 == 0:
                         output_model_file = './models/bert-base-uncased.bin.tmp'
-                        saved_dict = {'params1' : model1.module.state_dict()}
+                        saved_dict = {'params1': model1.module.state_dict()}
                         saved_dict['params2'] = model2.state_dict()
                         torch.save(saved_dict, output_model_file)
             except Exception as err:
                 traceback.print_exc()
-                if mode == 'bundle':   
-                    print(batch._id) 
+                if mode == 'bundle':
+                    print(batch._id)
     return (model1, model2)
 
 
-def main(output_model_file = './models/bert-base-uncased.bin', load = False, mode = 'tensors', batch_size = 2,
-            num_epoch = 3, gradient_accumulation_steps = 1, lr1 = 1e-5, lr2 = 1e-5, alpha = 0.2):
-    
-    BERT_MODEL = 'bert-base-uncased' # bert-large is too large for ordinary GPU on task #2
+def main(output_model_file='./models/bert-base-uncased.bin', load=False, mode='tensors', batch_size=6,
+         num_epoch=1, gradient_accumulation_steps=1, lr1=1e-4, lr2=1e-4, alpha=0.2):
+    BERT_MODEL = 'bert-base-uncased'  # bert-large is too large for ordinary GPU on task #2
     tokenizer = BertTokenizer.from_pretrained(BERT_MODEL, do_lower_case=True)
-    with open('./hotpot_train_v1.1_refined.json' ,'r') as fin:
+    with open('./hotpot_train_v1.1_refined.json', 'r') as fin:
         dataset = json.load(fin)
     bundles = []
     for data in tqdm(dataset):
@@ -147,18 +146,10 @@ def main(output_model_file = './models/bert-base-uncased.bin', load = False, mod
 
     else:
         model1 = BertForMultiHopQuestionAnswering.from_pretrained(BERT_MODEL,
-                cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(-1))
+                                                                  cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(
+                                                                      -1))
         model2 = CognitiveGNN(model1.config.hidden_size)
 
-    print('Start Training... on {} GPUs'.format(torch.cuda.device_count()))
-    model1 = torch.nn.DataParallel(model1, device_ids = range(torch.cuda.device_count()))
-    model1, model2 = train(bundles, model1=model1, device=device, mode=mode, model2=model2, # Then pass hyperparams
-        batch_size=batch_size, num_epoch=num_epoch, gradient_accumulation_steps=gradient_accumulation_steps,lr1=lr1, lr2=lr2, alpha=alpha)
-    
-    print('Saving model to {}'.format(output_model_file))
-    saved_dict = {'params1' : model1.module.state_dict()}
-    saved_dict['params2'] = model2.state_dict()
-    torch.save(saved_dict, output_model_file)
 
 import fire
 if __name__ == "__main__":
---
+++ tiny.py
@@ -0,0 +1,16 @@
+import torch
+from pytorch_transformers import BertTokenizer,BertModel
+
+tokenizer = BertTokenizer.from_pretrained('/home/shaoai/CogQA/uncased_L-2_H-128_A-2')
+model = BertModel.from_pretrained('/home/shaoai/CogQA/uncased_L-2_H-128_A-2')
+
+input_ids = torch.tensor(tokenizer.encode("Which magazine was started first Arthur's Magazine or First for Women")).unsqueeze(0)  # Batch size 1
+outputs = model(input_ids)
+
+# last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+sequence_output = outputs[0]
+pooled_output = outputs[1]
+print(sequence_output)
+print(pooled_output)
+print(sequence_output.shape)    ## 字向量
+print(pooled_output.shape)      ## 句向量
